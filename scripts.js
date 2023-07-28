518250678ad77ac95d324d1c84111b2423241dbd
kliffa <knielsvankleef@gmail.com>
2023-07-28 11:07:06 +0200

    add html structure of calculator

--- scripts.js
+++ scripts.js
@@ -1,15 +1,20 @@
-const add = function (a, b) {
-    return a + b;
-  };
+const add = (a, b) =>  a + b;
   
-const subtract = function (a, b) {
-    return a - b;
-  };
+const subtract = (a, b) => a - b;
 
-const multiply = function(a,b){
-    return a*b;
-};
+const multiply = (a,b) => a*b;
 
-const divide = function(a,b){
-    return a/b;
+const divide = (a,b)=>a/b;
+
+let a=0;
+let b=0;
+let operator="";
+
+function operate(a,b=0,operation=""){ // b=0 to ensure if operate is called when only a is something and operate = "" -> run a+0 
+    if(b===0 && operation==""){
+        return a;
+    }else if(b===0 && operation =="/"){
+        return "Syntax Error";
+    } 
+    
 }
